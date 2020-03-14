68eba556ef75c214a5bf4f7e560e1f88d0966cb3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-14 10:22:32 +0200

    no message

--- src/Blauhaus.Push.Server.AppCenter/_Ioc/IocServiceExtensions.cs
+++ src/Blauhaus.Push.Server.AppCenter/_Ioc/IocServiceExtensions.cs
@@ -1,17 +1,16 @@
 ﻿using Blauhaus.HttpClientService._Ioc;
 using Blauhaus.Ioc.Abstractions;
-using Blauhaus.Loggers.Console._Ioc;
 using Blauhaus.Push.Common.Abstractions;
 using Blauhaus.Push.Server.AppCenter.Service;
 
 namespace Blauhaus.Push.Server.AppCenter._Ioc
 {
-    public static class IocRegisteration
+    public static class IocServiceExtensions
     {
         public static IIocService RegisterAppCenterPushNotificationsServer(this IIocService iocService)
         {
             iocService.RegisterImplementation<IPushNotificationServerService, AppCenterPushNotificationServerService>();
-            iocService.RegisterHttpService();
+            iocService.RegisterClientHttpService();
             return iocService;
         }
     }
